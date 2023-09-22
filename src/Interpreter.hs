d597cd90573068b5ff03d6f3e8bcf0db3d7bd251
Joshua Passos <joshua.passos@cubos.io>
2023-09-22 18:10:22 -0300

    add tuple

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -26,13 +26,20 @@ data BaseType
   = VInt Int
   | VStr String
   | VBool Bool
+  | VTuple (BaseType, BaseType)
   | VFunction ([String], Term)
-  deriving (Show)
+
+instance Show BaseType where
+  show (VInt s) = show s
+  show (VBool s) = show s
+  show (VStr s) = show s
+  show (VTuple (a,b)) = "(" <> show a <> ", " <> show b <> ")"
+  show _ = "<Function>"
 
 evalAST :: AST -> StateT StateB IO BaseType
 evalAST (File {name = name, expression = expression}) = do
   state <- get
-  liftIO $ putStrLn ("Running " ++ name)
+  liftIO $ putStrLn ("Running " <> name)
   lift $ evalStateT (evalTerm expression) state
 
 evalInt :: TInt -> BaseType
@@ -68,7 +75,7 @@ evalTerm (TVar s) = do
   let vall = Map.lookup s' state
   case vall of
     Just value -> return value
-    Nothing -> error ("Var `" ++ s' ++ "` not defined")
+    Nothing -> error ("Var `" <> s' <> "` not defined")
 evalTerm (Function (Function' {value = value, parameters = parameters})) = do
   return $ VFunction (getParameters parameters, value)
   where
@@ -84,15 +91,29 @@ evalTerm (Call (Call' {callee = TVar callee, arguments = arguments})) = do
       l' <- traverse sequence l
       let state' = Map.fromList l' `Map.union` state
       lift $ evalStateT (evalTerm value) state'
-    _ -> error ("Var `" ++ s' ++ "` not defined")
+    _ -> error ("Var `" <> s' <> "` not defined")
 evalTerm (If (If' {condition = condition, then' = then', else' = else'})) = do
   condition' <- evalTerm condition
   case condition' of
     VBool True -> evalTerm then'
     VBool False -> evalTerm else'
-    _ -> error ("Invalid condition: `" ++ show condition' ++ "`")
+    _ -> error ("Invalid condition: `" <> show condition' <> "`")
+evalTerm (First (First' {value = value})) = do
+  value' <- evalTerm value
+  case value' of
+    VTuple (a,_) -> return a
+    _ -> error ("Invalid first: `" <> show value' <> "`")
+evalTerm (Second (Second' {value = value})) = do
+  value' <- evalTerm value
+  case value' of
+    VTuple (_,b) -> return b
+    _ -> error ("Invalid second: `" <> show value' <> "`")
+evalTerm (TTuple (Tuple' {first = first, second = second})) = do
+  first' <- evalTerm first
+  second' <- evalTerm second
+  return $ VTuple (first', second')
 evalTerm x = do
-  lift $ error ("Not implemented term: " ++ show x)
+  lift $ error ("Not implemented term: " <> show x)
 
 evalBinary :: BinaryOP -> BaseType -> BaseType -> BaseType
 evalBinary Add (VInt a) (VInt b) = VInt (a + b)
@@ -106,7 +127,7 @@ evalBinary Lte (VInt a) (VInt b) = VBool (a <= b)
 evalBinary Gte (VInt a) (VInt b) = VBool (a >= b)
 evalBinary Eq (VInt a) (VInt b) = VBool (a == b)
 evalBinary Neq (VInt a) (VInt b) = VBool (a /= b)
-evalBinary op _ _ = error ("Not implemented binary " ++ show op)
+evalBinary op _ _ = error ("Not implemented binary " <> show op)
 
 evalPrint :: Print -> StateT StateB IO BaseType
 evalPrint Print' {value = value} = do
@@ -118,16 +139,11 @@ evalPrint Print' {value = value} = do
     TVar (Var' {text = text}) -> do
       let vall = Map.lookup text state
       case vall of
-        Nothing -> error ("Var `" ++ text ++ "` not defined")
+        Nothing -> error ("Var `" <> text <> "` not defined")
         Just value -> return value
     v -> evalTerm v
-  lift $ printBasicTypes r
+  lift $ print r
   return r
-  where
-    printBasicTypes (VInt s) = print s
-    printBasicTypes (VBool s) = print s
-    printBasicTypes (VStr s) = print s
-    printBasicTypes _ = print "<Function>"
 
 execute :: Either (ParseError String) AST -> IO ()
 execute (Right s) = do
